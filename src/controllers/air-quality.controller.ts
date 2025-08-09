4fa6d4ce7cbf6de2436f6ae62eec7bf12678f302
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-10 01:02:33 +0300

    Feat: add getMostPollutedDate handler and route

--- src/controllers/air-quality.controller.ts
+++ src/controllers/air-quality.controller.ts
@@ -1,6 +1,8 @@
 import { RequestHandler } from "express";
 import { fetchAirQuality } from "../services/api/airQualityApiService";
 import { mapAirQualityToApi } from "../utils/airQualityApiMapper";
+import { selectMostPollutedDate } from "../services/db/airQualityDbService";
+
 
 
 // @desc       list air quality for lat and long
@@ -20,5 +22,27 @@ export const getAirQuality: RequestHandler = async(
     res.json({ success: true, message: 'air quality successful', result });
   } catch (error) {
     res.json({success: false, message: 'air quality Failed', error });
+  }
+}
+
+
+// @desc       get datetime where the paris zone is the most polluted
+// @route      GET api/v1/air_quality/paris/most_pulluted_date
+// @access     Public
+export const getMostPollutedDate: RequestHandler = async(
+  req, 
+  res: { json: (arg0: { success: boolean; message: string; result?: any; error?: any; }) => void; },
+  next) => {
+
+  const { city } = req.params;
+  const formattedCity = city.charAt(0).toUpperCase() + city.slice(1) // Capitalize the first letter
+
+  try {
+    const mostPollutedDate = await selectMostPollutedDate(formattedCity);
+    console.log('========== ',mostPollutedDate);
+  
+    res.json({ success: true, message: 'found most polluted date successfully', result: mostPollutedDate });
+  } catch (error) {
+    res.json({success: false, message: 'didn\'t find most polluted date successfully', error: error.message });
   }
 }
--- src/routes/air-quality.routes.ts
+++ src/routes/air-quality.routes.ts
@@ -1,9 +1,10 @@
-import { getAirQuality } from '../controllers/air-quality.controller';
+import { getAirQuality, getMostPollutedDate } from '../controllers/air-quality.controller';
 import express from 'express';
 const router = express.Router();
 
 
 // routes
 router.get('/', getAirQuality);
+router.get('/:city/most_polluted_date', getMostPollutedDate);
 
 export default router;
