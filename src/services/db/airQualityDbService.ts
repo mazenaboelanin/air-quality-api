faf301e4cc252bf8b2c13357579e360b1ef4352e
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-10 00:58:52 +0300

    Feat: add selectMostPollutedDate db query

--- src/services/db/airQualityDbService.ts
+++ src/services/db/airQualityDbService.ts
@@ -10,3 +10,19 @@ export async function createAirQualityRecord(data: any) {
   console.log(`==== Air quality record created successfully for ${newAirQuality.city}, ${newAirQuality.country}`);
   return newAirQuality;
 }
+
+
+export async function selectMostPollutedDate(city: string){
+  const mostPolluted = await AirQuality.findOne({ city })
+                                       .sort({ 'pollution.aqius': -1 })
+                                       .select('pollution.ts -_id');
+
+  if(!mostPolluted) {
+    throw new Error('didn\'t find most polluted date successfully');
+  }
+
+  const mostPollutedDate = mostPolluted.pollution.ts;
+
+  return mostPollutedDate;
+
+}
