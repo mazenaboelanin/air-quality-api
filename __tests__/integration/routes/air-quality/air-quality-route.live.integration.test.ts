d81d5176be2e1b6b250514c59f3f8a94ab852e22
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-12 19:35:56 +0300

    Feat: integration tests for air quality route live and mocked

---
+++ __tests__/integration/routes/air-quality/air-quality-route.live.integration.test.ts
@@ -0,0 +1,58 @@
+//  command for running live tests => npx jest --runInBand --testPathPatterns=live
+
+import request from "supertest";
+import app from '../../../../src/app';
+
+describe('GET /api/v1/air_quality?lat=2&lon=4', () => {
+  const OLD_ENV = process.env;
+
+  beforeEach(() => {
+    jest.resetModules(); // Clears the require cache
+    process.env = { ...OLD_ENV }; // reset OLD_ENV
+  });
+  
+  afterAll(() => {
+    process.env = OLD_ENV; // restore OLD_ENV after overwriting 
+  });
+
+  it("should return live air quality data from external API", async () => {
+    const res = await request(app)
+      .get("/api/v1/air_quality?lat=48.859425&lon=2.351666"); // Paris
+
+      // console.log("⚠️ Live API returned:", res.body);
+      expect(res.status).toBe(200);
+      expect(res.body.success).toBe(true);
+      expect(res.body.result).toHaveProperty("pollution");
+  }, 10000); // Increase timeout ( default timeout of 5 seconds (5000 ms)
+
+  it("should return Error if no Lat or Lon Specified", async () => {
+    const urls = [
+      '/api/v1/air_quality?lat=48.859425',
+      '/api/v1/air_quality?lon=2.351666',
+      '/api/v1/air_quality'
+    ];
+
+    for (const url of urls) {
+      const res = await request(app).get(url);
+
+      // console.log(`⚠️ Live API returned: for url => ${url}`, res.body);
+      expect(res.status).toBe(400);
+      expect(res.body.success).toBe(false);
+      expect(res.body.message).toBe("air quality Failed");
+      expect(res.body.error).toBe("you should enter lat and lon");
+    };
+  }, 10000); // Increase timeout ( default timeout of 5 seconds (5000 ms)
+
+
+  it("should return 400 if external API is down (live)", async () => {
+    process.env.IQAIR_API_KEY = "1234";
+    const res = await request(app)
+      .get("/api/v1/air_quality?lat=48.8566&lon=2.3522"); // Paris
+  
+    // console.log(`⚠️ Live API returned:`, res.body);
+    expect(res.status).toBe(400);
+    expect(res.body.success).toBe(false);
+    expect(res.body.error).toBe("Could not fetch air quality data");
+  }, 15000);
+
+});
---
+++ __tests__/integration/routes/air-quality/air-quality-route.mocked.integration.test.ts
@@ -0,0 +1,40 @@
+// __tests__/integration/api/airQuality.mocked.int.test.ts
+import request from "supertest";
+import app from "../../../../src/app";
+import { fetchAirQuality }from "../../../../src/services/api/air-quality-api.service";
+
+// Factories
+import { airQualityApiResponseFactory } from "../../../factories/air-quality/air-quality-api-response.factory";
+
+// Mocks
+jest.mock('../../../src/services/api/air-quality-api.service');
+
+describe("GET /api/v1/air_quality (mocked external API)", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should return air quality data (mocked)", async () => {
+    const apiResponseFixture = airQualityApiResponseFactory().data;
+    (fetchAirQuality as jest.Mock).mockResolvedValue(apiResponseFixture);
+
+    const res = await request(app)
+      .get("/api/v1/air_quality?lat=48.859425&lon=2.351666");
+
+    expect(res.status).toBe(200);
+    expect(res.body.success).toBe(true);
+    expect(res.body.result).toHaveProperty("pollution");
+  });
+
+  it("should return error if external API throws", async () => {
+    (fetchAirQuality as jest.Mock).mockRejectedValue(new Error("Could not fetch air quality data"));
+
+    const res = await request(app)
+      .get("/api/v1/air_quality?lat=48.859425&lon=2.351666");
+
+    expect(res.status).toBe(400); 
+    expect(res.body.success).toBe(false);
+    expect(res.body.message).toBe("air quality Failed");
+    expect(res.body.error).toBe("Could not fetch air quality data");
+  });
+});
