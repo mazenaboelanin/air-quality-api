556677967f6ec15d348bc97f7455075b8eb34816
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-13 03:08:31 +0300

    Refactor: rename schedules to jobs

--- src/jobs/scheduler.ts
+++ src/jobs/scheduler.ts
@@ -1,15 +1,16 @@
 import cron from 'node-cron';
 import { checkAirQualityJob } from './check-air-quality.job';
 
-const schedules = [
+const jobs = [
   {
+    name: 'Check Air Quality Job',
     job: checkAirQualityJob,
     schedule: '*/15 * * * * *'
   }
 ];
 
 export function runScheduler() {
-  schedules.forEach(({ schedule: cronExpression, job }) => {
+  jobs.forEach(({ schedule: cronExpression, job }) => {
     cron.schedule(cronExpression, async () => {
       try {
         console.log('==== Cron job is running every minute!', new Date().toISOString());
