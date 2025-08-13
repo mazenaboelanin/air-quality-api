f6817a1f961ca29ea99e59a50d5861c0c280404e
Mazen Abo Elanin Ahmed <39408280+mazenaboelanin@users.noreply.github.com>
2025-08-13 03:03:27 +0300

    Feat: handle wrong routes

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import express from 'express';
 
 // importing Routes
 import airQualityRoute from './routes/air-quality.routes';
+import { StatusCodes } from 'http-status-codes';
 
 
 const app = express();
@@ -11,5 +12,12 @@ app.use(express.json());
 
 // setup routes
 app.use('/api/v1/air_quality', airQualityRoute);
+
+app.use((req, res) => {
+    res.status(StatusCodes.NOT_FOUND).json({
+      success: false,
+      message: 'Route not found',
+    });
+  });
 
 export default app;
